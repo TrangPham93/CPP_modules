cbf676f99a2cba295620972e6c06137bdf2d5b4b
Trang Pham <trpham@c1r2p6.hive.fi>
2025-08-04 19:11:53 +0300

    fix ex01

--- C00/ex01/srcs/PhoneBook.cpp
+++ C00/ex01/srcs/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 15:52:43 by trpham            #+#    #+#             */
-/*   Updated: 2025/08/04 18:52:03 by trpham           ###   ########.fr       */
+/*   Updated: 2025/08/04 19:09:07 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ PhoneBook::PhoneBook()
 
 PhoneBook::~PhoneBook(){}
 
+static std::string truncate_to_ten(std::string str)
+{	
+	return (str.length() <= 10 ? str : str.substr(0, 9) + ".");
+}
+
 static void	display_contact(Contact *ContactList, int search_index)
 {
 	std::cout << std::right << std::setw(10) << "Index" << "|"
@@ -26,9 +31,9 @@ static void	display_contact(Contact *ContactList, int search_index)
 		<< std:: right << std::setw(10) << "Last name" << "|" 
 		<< std:: right << std::setw(10) << "Nickname" << std::endl;
 	std::cout << std::right << std::setw(10) << search_index << "|" 
-		<< std::right << std::setw(10) << ContactList[search_index].FirstName << "|" 
-		<< std::right << std::setw(10) << ContactList[search_index].LastName << "|"
-		<< std::right << std::setw(10) << ContactList[search_index].NickName << std::endl;
+		<< std::right << std::setw(10) << truncate_to_ten(ContactList[search_index].FirstName) << "|" 
+		<< std::right << std::setw(10) << truncate_to_ten(ContactList[search_index].LastName) << "|"
+		<< std::right << std::setw(10) << truncate_to_ten(ContactList[search_index].NickName) << std::endl;
 }
 
 static bool	validate_index(std::string search_string)

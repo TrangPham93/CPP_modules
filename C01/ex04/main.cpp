946310279fb8da175945d2e1a55270c7ee4ea1c4
TrangPham93 <hatrangc2@gmail.com>
2025-08-10 00:58:43 +0300

    set s1 condition to not empty string, ex04

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 13:25:48 by trpham            #+#    #+#             */
-/*   Updated: 2025/08/09 23:51:43 by trpham           ###   ########.fr       */
+/*   Updated: 2025/08/10 00:49:56 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,29 @@ static void	ft_replace(std::string* read_str,
 	return ;
 }
 
+/*  write infile to outfile */
+static void	write_infile_to_outfile(std::ifstream& infile, 
+	std::ofstream& outfile, std::string s1, std::string s2)
+{
+	std::string		read_str;
+	
+	while (std::getline(infile, read_str)) // while reading not fail
+	{
+		ft_replace(&read_str, s1, s2);
+		outfile << read_str << std::endl;
+	}
+	if (!infile.eof())
+	{
+		std::cout << "Error: read file failed" << std::endl;
+		return ;
+	}
+}
+
 int	main(int ac, char **av)
 {
 	std::string		filename;
 	std::string		s1;
 	std::string		s2;
-	std::string		read_str;
 	
 	if (ac != 4)
 	{
@@ -45,6 +62,11 @@ int	main(int ac, char **av)
 
 	filename = std::string(av[1]);
 	s1 = std::string(av[2]);
+	if (s1.empty())
+	{
+		std::cout << "s1 cannot be empty string" << std::endl;
+		return (EXIT_FAILURE);
+	}
 	s2 = std::string(av[3]);
 	
 	// open infile
@@ -62,7 +84,6 @@ int	main(int ac, char **av)
 	{
 		std::cout << new_file_name << " already exists, cannot recreate" 
 			<< std::endl;
-		checkfile.close();
 		return (EXIT_FAILURE);
 	}
 	
@@ -71,26 +92,10 @@ int	main(int ac, char **av)
 	if (!outfile.is_open())
 	{
 		std::cout << filename << ".replace cannot be created" << std::endl;
-		infile.close();
 		return (EXIT_FAILURE);
 	}
 	
-	// write infile to outfile
-	while (std::getline(infile, read_str)) // while reading not fail
-	{
-		ft_replace(&read_str, s1, s2);
-		outfile << read_str << std::endl;
-	}
-	if (!infile.eof())
-	{
-		std::cout << "Error: read file failed" << std::endl;
-		infile.close();
-		outfile.close();
-		return (EXIT_FAILURE);
-	}
-	
-	infile.close();
-	outfile.close();
+	write_infile_to_outfile(infile, outfile, s1, s2);
 	
 	return (EXIT_SUCCESS);
 }

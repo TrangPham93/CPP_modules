932824c83915a6bcf640e36a17982440e0c5fa61
Trang Pham <trpham@c1r2p6.hive.fi>
2025-08-11 16:53:46 +0300

    add ex01

---
+++ C02/ex01/Fixed.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/11 11:35:49 by trpham            #+#    #+#             */
+/*   Updated: 2025/08/11 15:58:16 by trpham           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int Fixed::_fractionBit = 8;
+
+Fixed::Fixed(void): _rawValue{0}
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed& Fixed::operator = (const Fixed& copy)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &copy) //checks if the object you are assigning to is different from the object you are assigning from.
+		this->_rawValue = copy.getRawBits();
+	return (*this);
+}
+
+int		Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _rawValue;
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+	_rawValue = raw;
+}

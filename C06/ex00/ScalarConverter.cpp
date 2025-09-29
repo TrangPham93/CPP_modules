23aa2327fcba4eda4d1577dd5c7597b0bd941f95
Trang Pham <trpham@c1r2p6.hive.fi>
2025-09-29 11:59:16 +0300

    add ex00

--- C06/ex00/ScalarConverter.cpp
+++ C06/ex00/ScalarConverter.cpp
@@ -19,19 +19,21 @@ ScalarConverter::~ScalarConverter()
 static void	printChar(std::string str)
 {
 	std::cout << "char: " << str << std::endl;
-	std::cout << "int: " << str[0] + '0' << std::endl;
-	std::cout << "float: impossible" << std::endl;
-	std::cout << "double: impossible" << std::endl;
+	std::cout << "int: " << static_cast<int>(str[0]) << std::endl;
+	std::cout << "float: " << static_cast<float>(str[0]) << std::endl;
+	std::cout << "double: " << static_cast<float>(str[0]) << std::endl;
 	
 }
 
-static void	printDigit(std::string str)
+static void	printDigit(int num)
 {
-	std::cout << "char: " << str << std::endl;
-	std::cout << "int: " << str[0] - '0' << std::endl;
-	// float fNumber = std::stof(str);
-	std::cout << "float: " << static_cast<float>(std::stof(str)) << std::endl;
-	std::cout << "double: " << std::stod(str) << std::endl;
+	if (num < 32 || num > 126)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: Non displayable" << static_cast<char>(num) << std::endl;
+	std::cout << "int: " << static_cast<int>(num) << std::endl;
+	std::cout << "float: " << static_cast<float>(num) << std::endl;
+	std::cout << "double: " << static_cast<double>(num) << std::endl;
 	
 }
 
@@ -39,26 +41,32 @@ void ScalarConverter::convert (std::string str)
 {
 	// check what is the input string
 	// std::cout << str << std::endl; //convert str to double
-	if (str.length() == 1)
-	{
-		// if (str.length() != 3)
-		// 	throw std::runtime_error("Only handle 1 character");
+	// if (str.length() == 1)
+	// {
+	// 	// if (str.length() != 3)
+	// 	// 	throw std::runtime_error("Only handle 1 character");
 		
-		if (str[0] < 32 || str[0] > 126)
-			throw std::runtime_error("Non-displayable character");
-		if (str[0] > 48 && str[0] < 57)
-			printDigit(str);
-		else
-		{
-			printChar(str);
-		}
-	}
-	else
-	{
-		printDigit(str);
-		// std::cout << "no thing" << std::endl; //convert str to double
+	// 	if (str[0] < 32 || str[0] > 126)
+	// 		throw std::runtime_error("Non-displayable character");
+	// 	if (str[0] > 48 && str[0] < 57)
+	// 		printDigit(str);
+	// 	else
+	// 	{
+	// 		printChar(str);
+	// 	}
+	// }
+	// else
+	// {
+	// 	printDigit(str);
+	// 	// std::cout << "no thing" << std::endl; //convert str to double
 		
-	}
+	// }
 	// std::stof(str); //convert str to float
+	int	num = 0;
+	for (int i = 0; i < str.length(); i++)
+	{
+		num = num * 10 + str[i];
+	}
+	printDigit(num);
 	
 }

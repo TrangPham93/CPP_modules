1109d03efb495a2dec60088387f0a9800bc4d975
TrangPham93 <hatrangc2@gmail.com>
2025-10-01 22:38:49 +0300

    finish ex02

--- C06/ex02/main.cpp
+++ C06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 20:02:08 by trpham            #+#    #+#             */
-/*   Updated: 2025/10/01 21:49:11 by trpham           ###   ########.fr       */
+/*   Updated: 2025/10/01 22:38:03 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,30 @@ int main()
 {
 	// Get a different random number each time the program runs
 	srand(time(0));
-	try
-	{
-		Base * newBase = generate();
-		identify(newBase);
-		identify(*newBase);
+	
+	std::cout << "\n--------test-1---------\n" << std::endl;
+	Base * newBase = generate();
+	identify(newBase);
+	identify(*newBase);
 
-		Base* nullBase = nullptr;
-		identify(nullBase);
+	Base* nullBase = nullptr;
+	identify(nullBase);
+	
+	delete newBase;
+
+	std::cout << "\n--------test-2---------\n" << std::endl;
+
+	Base	unknow;
+	A		a;
+	B		b;
+	identify(unknow);
+	identify(a);
+	identify(b);
+	
+	std::cout << "\n--------test-3---------\n" << std::endl;
+
+	Base&	refObj = a;
+	identify(refObj);
 		
-		delete newBase;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
 	return 0;
 }

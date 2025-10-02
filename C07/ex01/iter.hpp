ab6115f4c4fdf326b5cff153e6437f233b55f388
TrangPham93 <hatrangc2@gmail.com>
2025-10-02 21:27:49 +0300

    add all

--- C07/ex01/iter.hpp
+++ C07/ex01/iter.hpp
@@ -6,15 +6,23 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 11:50:36 by trpham            #+#    #+#             */
-/*   Updated: 2025/10/02 12:48:07 by trpham           ###   ########.fr       */
+/*   Updated: 2025/10/02 21:26:30 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
 
-template <typename T>
-void iter(std::string address, int len, func(T))
+
+/* 	function pointer:
+	f is a pointer to a function that: takes a parameter of type T& and 
+	returns void. 
+	void iter(T* arr, unsigned int len, void (*f)(T&)) -> 
+	F can be a function pointer, lambda, or function template instantiated.
+	No need to match exact function pointer type — the compiler deduces it. */
+template <typename T, typename F>
+void iter(T* arr, unsigned int len, F f)
 {
-	
+	for (unsigned int i = 0; i < len; i++)
+		f(arr[i]); // equal to (*f)(arr[i]) 
 }
--- C07/ex01/main.cpp
+++ C07/ex01/main.cpp
@@ -6,23 +6,57 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 11:15:34 by trpham            #+#    #+#             */
-/*   Updated: 2025/10/02 12:47:32 by trpham           ###   ########.fr       */
+/*   Updated: 2025/10/02 21:18:51 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int	multiply5(int x)
+void	multiply5(int& x)
 {
-	return x * 5;
+	x = x * 5;
 }
 
+void	capitalizeStr(std::string& s)
+{
+	for (unsigned int i = 0; i < s.length(); i++)
+	{
+		s[i] = toupper(s[i]);
+	}
+}
+
+template <typename T> 
+void printMe(T x)
+{
+	std::cout << "type: " << typeid(x).name() << " and value: " << x << std::endl;
+}
 
 int main( void ) 
 {
+	std::cout << "\n------------int------------\n" << std::endl; 
 	int arr[3] = {2, 0, 100};
 	
-	::iter(&arr, 3, multiply5());
+	::iter(arr, 3, multiply5);
+	
+	for (int i = 0; i < 3; i++)
+		std::cout << arr[i] << std::endl;
+	
+	std::cout << "\n------------string------------\n" << std::endl; 
+	std::string* strPtr = new std::string[4]{"don't", "look", "up", "!!"};
+	::iter(strPtr, 4, capitalizeStr);
+	
+	for (int i = 0; i < 4; i++)
+		std::cout << strPtr[i] << std::endl;
+
+	delete[] strPtr;
+	
+	std::cout << "\n-------function-template------\n" << std::endl;
+	float arrFloat[100];
+	for (int i = 0; i < 100; i++)
+		arrFloat[i] = i;
+
+	::iter(arrFloat, 100, printMe<float>);
+	
 	
 	return 0;
 }

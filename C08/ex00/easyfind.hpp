63463fdeb80055237db69beccaef87dce1902c2f
TrangPham93 <hatrangc2@gmail.com>
2025-10-06 13:27:53 +0300

    use iterator for ex00

--- C08/ex00/easyfind.hpp
+++ C08/ex00/easyfind.hpp
@@ -6,27 +6,28 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 11:59:19 by trpham            #+#    #+#             */
-/*   Updated: 2025/10/06 12:34:36 by trpham           ###   ########.fr       */
+/*   Updated: 2025/10/06 13:19:54 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
 #include <array>
+#include <vector>
+#include <algorithm>
 
 /* STL containers are divided into the following categories:
    1. Sequence Containers: implement linear data structures in which the elements can be accessed sequentially: array, vector, deque, list, forward list.
    2. Associative Containers: store data in some sorted order: set, map, multiset, multimaps.
    3. Unordered Containers: implement unsorted hashed data structures that can be quickly searched: unordered set, unordered map, unordered multiset, unordered multimap
-   4. Container Adapters: provide a different interface for other containers. They adapt the behavior of underlying containers to fit specific use cases. */
-
+   4. Container Adapters: provide a different interface for other containers. They adapt the behavior of underlying containers to fit specific use cases. 
+   
+   --> An iterator in C++ is a pointer-like object that points to an element of the STL container. They are generally used to loop through the contents of the STL container in C++. The main advantage of STL iterators is that they make the STL algorithms independent of the type of container used. We can just pass the iterator to the container elements instead of the container itself to the STL algorithms */
 template <typename T>
-int easyfind(T t, int num)
+typename T::iterator easyfind(T& t, int num)
 {
-	for (unsigned int i = 0; i < t.size(); i ++)
-	{
-		if (t.at(i) == num)
-			return i;
-	}
+	auto it = std::find(t.begin(), t.end(), num);
+	if (it != t.end())
+		return (it);
 	throw std::runtime_error("There is no occurence of the number " + std::to_string(num));
 }
--- C08/ex00/main.cpp
+++ C08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 11:15:34 by trpham            #+#    #+#             */
-/*   Updated: 2025/10/06 12:37:03 by trpham           ###   ########.fr       */
+/*   Updated: 2025/10/06 13:26:04 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,45 @@
 
 int main( void ) 
 {
+	std::cout << "\n---------array------------\n\n";
 	std::array<int,6> arr = {1, 0, 3, 100, 5, -2};
 	
 	try
 	{
-		int pos = easyfind(arr, 5);
+		std::array<int,6>::iterator pos = easyfind(arr, 5);
 		std::cout << "first occurance of 5: " << pos << std::endl;
+		std::cout << "first occurance of 5: " << *pos << std::endl;
 
-		int nePos = easyfind(arr, -2);
-		std::cout << "first occurance of -2: " << nePos << std::endl;
+		std::array<int,6>::iterator nepos = easyfind(arr, -2);
+		std::cout << "first occurance of -2: " << nepos << std::endl;
+		std::cout << "first occurance of -2: " << *nepos << std::endl;
 		
-		int noPos = easyfind(arr, -1);
-		std::cout << "first occurance of -1: " << noPos << std::endl;
+		std::array<int,6>::iterator nopos = easyfind(arr, -1);
+		std::cout << "first occurance of -1: " << nopos<< std::endl;
+		
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "\n---------vector------------\n\n";
+	std::vector<int> vec = {1, 0, 3, 100, 5, -2};
+	
+	try
+	{
+		// Iterator of vector container: This iterator is a wrapper around a pointer, not a pointer itself.
+		// It behaves like a pointer (supports *, ++, etc.),
+		// but you can’t directly print it — because std::ostream (<<) doesn’t
+		// know how to handle it.
+		std::vector<int>::iterator pos = easyfind(vec, 5);
+		std::cout << "first occurance of 5: " << *pos << std::endl;
+
+		std::vector<int>::iterator nepos = easyfind(vec, -2);
+		std::cout << "first occurance of -2: " << *nepos << std::endl;
+		
+		std::vector<int>::iterator nopos = easyfind(vec, -1);
+		std::cout << "first occurance of -1: " << *nopos<< std::endl;
 		
 	}
 	catch(const std::exception& e)
